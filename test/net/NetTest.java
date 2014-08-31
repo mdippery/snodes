mdippery/snodes

commit 0b7553a1d4811446a256104ad9966b70746a92c2

Fixed one of the network tests.
The network test is pretty outdated, and as work progresses, I'll
probably end up getting rid of it. I had to comment out a whole block of
code to get the test to work, and even so, it still throws a bunch of
exceptions while executing. But it "works" for now, so I'll try to fix it
later.

test/net/NetTest.java
@@ -26,6 +26,7 @@ public final class NetTest
 		System.err.println("Running network tests...");
 		
 		SnodesServer server = SnodesServer.getInstance();
+        /*
 		ServerListener sl = new ServerListener() {
 			public void processConnection(SnodesConnection conn) {
 				System.out.println("Received new connection request: " + conn);
@@ -37,6 +38,7 @@ public final class NetTest
 			}
 		};
 		server.addListener(sl);
+        */
 		
 		server.start();
 		

.gitignore
@@ -2,3 +2,5 @@
 build
 Snodes.jar
 snodes.log
+test/fs/data
+test/**/*.class

commit 34676dde151e805a05b265f66b7a8f6329c67d6a

Fix network test
The network test was horribly outdated and needed a few changes to make
it work properly again. I actually believe it may have been broken in
0b7553a; setting up a ServerListener _may_ actually be the right way to
test the code. For now, though, I'll keep this as-is and maybe fine-tune
it down the road.

src/snodes/net/SnodesConnection.java
@@ -187,7 +187,6 @@ public class SnodesConnection
 		
 		this.id = id;
 		this.encryptKey = new EncryptionKey(key);
-		//this.status = Status.AUTHORIZED;
 	}
 	
 	/**

test/net/NetTest.java
@@ -6,6 +6,8 @@
 
 import snodes.net.*;
 
+import java.net.InetAddress;
+
 
 public final class NetTest
 {
@@ -14,26 +16,34 @@ public final class NetTest
             System.out.println("Got packet: " + packet.getType() + " from: " + conn);
         }
     };
-    
+
     private static final PacketFilter pf = new PacketFilter() {
         public boolean accept(Packet.Type type) {
             return true;
         }
     };
-    
+
     public static void main(String[] args) throws Exception
     {
         System.err.println("Running network tests...");
-        
+
+        final SnodesConnection local = new SnodesConnection("127.0.0.1");
+
+        ConnectionManager cm = new ConnectionManager() {
+            public SnodesConnection getConnection(InetAddress host) {
+                return local;
+            }
+        };
+
         SnodesServer server = SnodesServer.getInstance();
-        
+        server.setConnectionManager(cm);
         server.start();
-        
-        SnodesConnection local = new SnodesConnection("127.0.0.1");
+
         local.addListener(pl, pf);
         local.authenticate("mypasskey");
         local.connect();
-        
+        authorize(local);
+
         FileTransfer ft = local.createTransfer("myfile");
         PacketListener ftl = new PacketListener() {
             public void processPacket(SnodesConnection ft, Packet p) {
@@ -51,26 +61,26 @@ public final class NetTest
             }
         };
         local.addListener(ftl, ftf);
-        
+
         byte[] bytes = new byte[4];
         bytes[0] = bytes[1] = bytes[2] = bytes[3] = (byte) 94;
         ft.send(bytes, 1, 8L);
-    
+
         local.disconnect();
     }
-    
+
     private static void authorize(SnodesConnection conn) throws Exception
     {
-        byte[] key = new byte[4];
-        
-        for (int i = 92; i < 96; i++) {
+        byte[] key = new byte[8];
+
+        for (int i = 92; i < 92 + key.length; i++) {
             key[i-92] = (byte) i;
         }
-        
+
         conn.addListener(pl, pf);
         conn.authenticate("mypasskey");
         conn.authorize(1, key);
     }
-    
+
     private NetTest() {}
 }